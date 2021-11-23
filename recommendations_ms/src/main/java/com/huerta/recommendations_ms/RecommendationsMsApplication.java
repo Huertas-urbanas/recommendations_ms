4db8b957e928ddcc5d3a11021f3b90f16b959193
Manuel_Quintana <90158740+ManQui25@users.noreply.github.com>
2021-11-23 18:03:53 -0500

    feat: creacion del ms

---
+++ recommendations_ms/src/main/java/com/huerta/recommendations_ms/RecommendationsMsApplication.java
@@ -0,0 +1,13 @@
+package com.huerta.recommendations_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationsMsApplication.class, args);
+	}
+
+}
---
+++ recommendations_ms/src/test/java/com/huerta/recommendations_ms/RecommendationsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huerta.recommendations_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
